7d6f7fdc75262976e5377fc42dfdbfbb755e318d
arctic <icyfreeze5@gmail.com>
2014-04-25 17:23:29 -0700

    clean up some stuff

--- main.py
+++ main.py
@@ -34,27 +34,7 @@ currentGiftKey = read_string(serverStartResponse, 15, 16)
 myToken = http_new_get("http://l3.ajf.me/gg2/getToken.php?user_name=arctic&user_key=vk6lxQx3xkUBfiv")
 myLoadout = http_new_get("http://l3.ajf.me/gg2/getLoadout.php?token="+myToken+"&server_key="+serverKey)
 
-#other stuff
-# name1 = "123456"
-# key1 = "jFhwE3lHp9JLwBH"
-# token1 = http_new_get("http://l3.ajf.me/gg2/getToken.php?user_name="+name1+"&user_key="+key1)
-# loadout1 = http_new_get("http://l3.ajf.me/gg2/getLoadout.php?token="+token1+"&server_key="+serverKey)
-
-# name2 = "DrunkenSteve"
-# key2 = "wsgm03qVeG1K6xM"
-# token2 = http_new_get("http://l3.ajf.me/gg2/getToken.php?user_name="+name2+"&user_key="+key2)
-# loadout2 = http_new_get("http://l3.ajf.me/gg2/getLoadout.php?token="+token2+"&server_key="+serverKey)
-
-# name3 = "arctic2"
-# key3 = "spYU4WsINihCGpe"
-# token3 = http_new_get("http://l3.ajf.me/gg2/getToken.php?user_name="+name3+"&user_key="+key3)
-# loadout3 = http_new_get("http://l3.ajf.me/gg2/getLoadout.php?token="+token3+"&server_key="+serverKey)
-
-# name4 = "arctic2"
-# key4 = "spYU4WsINihCGpe"
-# token4 = http_new_get("http://l3.ajf.me/gg2/getToken.php?user_name="+name4+"&user_key="+key4)
-# loadout4 = http_new_get("http://l3.ajf.me/gg2/getLoadout.php?token="+token4+"&server_key="+serverKey)
-    
+#load other accounts
 for n in data.usernames[:len(data.usernames)]:
     http_new_get("http://l3.ajf.me/gg2/getLoadout.php?token="+n+"&server_key="+serverKey)
     print n
@@ -71,10 +51,10 @@ print "current_time:",current_time
 
 giftDelay = current_time+1000*60*15
 
-x = 1
-"""while x == 1:
-    if (int(round(time.time() * 1000)) > giftDelay):
-        attemptGift()"""
+# x = 1
+# while x == 1:
+    # if (int(round(time.time() * 1000)) > giftDelay):
+        # attemptGift()
 
 #Refresh Token
 if (startTime + 1000*60*60*2 < (int(round(time.time() * 1000)))):
@@ -82,31 +62,4 @@ if (startTime + 1000*60*60*2 < (int(round(time.time() * 1000)))):
     myToken = http_new_get("http://l3.ajf.me/gg2/refreshToken.php?user_name="+myName+"&user_key="+serverKey+"&token="+myToken)
     print "Token refreshed"
     print "New Token:",myToken
-
-#do stuff 
-
-# nope, this shit dont work
-#print 'ALIM is now harvesting items, please be patient and use this time to watch some anime or "other" things'
-#attemptGift()
-
-#TODO: print out message every time a gift is outputted
-
 raw_input()
-
-
-
-# C:\Users\Jason\Documents\GitHub\ItemServer-Harvester>main
-# 'main' is not recognized as an internal or external command,
-# operable program or batch file.
-
-# C:\Users\Jason\Documents\GitHub\ItemServer-Harvester>main.py
-  # File "C:\Users\Jason\Documents\GitHub\ItemServer-Harvester\main.py", line 89
-
-    # ^
-# SyntaxError: EOF while scanning triple-quoted string literal
-
-# C:\Users\Jason\Documents\GitHub\ItemServer-Harvester>main.py
-# Arctic's Lightweight Itemserver Miner Alpha v0.01
-# Server Start Response aMPFnu6Ff4fVPbzqFMAeCJIEtGcINQ
-# My Token: vW6cNZwXD1QW1b6
-# current_time: 1397794224334
